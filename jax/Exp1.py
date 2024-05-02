9e71d3292fd47ae4b4c54065c27cc0b3822c369c
vivz <vivianzhang61@gmail.com>
2024-05-02 11:21:32 -0400

    parameter updates in experiment 1

--- jax/Exp1.py
+++ jax/Exp1.py
@@ -14,10 +14,11 @@ import numpy as np
 #this function is for plotting the results of the experiment
 def plot_results(unstruct_errors, ortho_errors):
     import matplotlib.pyplot as plt
-    plt.plot([1000, 5000, 10000], unstruct_errors, label='Unstructured Random Matrix')
-    plt.plot([1000, 5000, 10000], ortho_errors, label='Orthogonal Random Matrix')
+    plt.plot([10, 20, 50, 80, 100, 120, 150, 200], unstruct_errors, label='Unstructured Random Matrix')
+    plt.plot([10, 20, 50, 80, 100, 120, 150, 200], ortho_errors, label='Orthogonal Random Matrix')
     plt.xlabel('Number of Random Features')
-    plt.ylabel('Relative Error')
+    plt.ylabel('MSE')
+    plt.yscale('log')
     plt.legend()
     plt.savefig('exp1.png')
 
@@ -28,18 +29,15 @@ def Exp1():
     # key -> [batch_size, dim1, dim2, ..., dimN, num_heads, mem_channels]
     # value -> [batch_size, dim1, dim2, ..., dimN, num_heads, value_channels]
     unstruct_errors, ortho_errors = [], []
-    for nb_random_features in [1000, 5000, 10000]:
-        qk_dim = 8
-        v_dim = 10
+    for nb_random_features in [10, 20, 50, 80, 100, 120, 150, 200]:
+        qk_dim = 10
+        v_dim = 10  # this might be sequence length?
         batch_size = 1
-        dim1 = 2
-        dim2 = 1
-        dim3 = 1
+        dim1 = 16  # this is embedding dimension? 
         num_heads = 1
-        #nb_random_features = 10000
-        shape_query = (batch_size, dim1, dim2, dim3, num_heads, qk_dim)
-        shape_key = (batch_size, dim1, dim2, dim3, num_heads, qk_dim)
-        shape_value = (batch_size, dim1, dim2, dim3, num_heads, v_dim)
+        shape_query = (batch_size, dim1, num_heads, qk_dim)
+        shape_key = (batch_size, dim1, num_heads, qk_dim)
+        shape_value = (batch_size, dim1, num_heads, v_dim)
         query = random.normal(random.PRNGKey(0), shape_query)
         key = random.normal(random.PRNGKey(0), shape_key)
         value = random.normal(random.PRNGKey(0), shape_value)
@@ -71,12 +69,8 @@ def Exp1():
         ortho_rfm_attention_result = fast_ortho_rfm_dot_product_attention.dot_product_attention(
             query, key, value)
 
-        unstruct_error = jnp.abs(
-            (standard_attention_result - unstruct_rfm_attention_result) /
-            standard_attention_result)
-        ortho_error = jnp.abs(
-            (standard_attention_result - ortho_rfm_attention_result) /
-            standard_attention_result)
+        unstruct_error = jnp.abs((standard_attention_result - unstruct_rfm_attention_result)**2)
+        ortho_error = jnp.abs((standard_attention_result - ortho_rfm_attention_result)**2)
         unstruct_errors.append(np.mean(unstruct_error))
         ortho_errors.append(np.mean(ortho_error))
     plot_results(unstruct_errors, ortho_errors)
