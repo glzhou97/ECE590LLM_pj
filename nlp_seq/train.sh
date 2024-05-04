36dcba3873694f31a4b1cde95b804710df5228b2
Chiyue Wei <1780277455@qq.com>
2024-05-03 20:13:33 -0400

    add exoeriment for using favor+ in transformer

---
+++ nlp_seq/train.sh
@@ -0,0 +1,4 @@
+python train.py --batch_size=64 --model_dir=./english \
+    --dev=/home/cw541/ud-treebanks-v2.0/UD_English/en-ud-dev.conllu \
+    --train=/home/cw541/ud-treebanks-v2.0/UD_English/en-ud-train.conllu \
+    --num_train_steps=1000 --eval_frequency=1
